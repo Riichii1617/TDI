aab0e0f3bfe550f1f77c300e6b025bc85cd8ee89
Jesus-RA <jesus.ra98@hotmail.com>
2020-02-23 16:15:45 -0600

    Se agrego la Base de datos

---
+++ registroUsuario.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registro usuario</title>
+</head>
+<body>
+	<form>
+		Usuario: <input type="text" name="usuario"> <br><br>
+		Contraseña: <input type="text" name="contrasena"><br><br>
+		<input type="submit" name="registrar" value="Registrar">
+	</form>
+</body>
+</html>
