e90aab4b40384c46c5a221bf50b53316faaa786c
Jesus-RA <jesus.ra98@hotmail.com>
2020-02-24 00:21:12 -0600

    Pruebas

--- Controllers/registroController.php
+++ Controllers/registroController.php
@@ -1,23 +1,18 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Registro usuario</title>
-	<meta name="viewport" content="width= device-width, user-scalable=yes initial-scale=1.0, maximun-scale=3.0, minimun-scale=1.0">
-</head>
-<body>
-	<form>
-		Usuario: <input type="text" name="usuario" placeholder="Nombre de usuario"> <br><br>
-		Contraseña: <input type="password" name="contrasena" placeholder="Contraseña"> <br><br>
-		E-mail: <input type="email" name="email" placeholder="Correo electrónico"><br><br>
-		<input type="submit" name="registrar" value="Registrar">
-	</form>
-	<?php
-		/*$user= $_REQUEST['usuario'];
-		$pass= $_REQUEST['contrasena'];
-		$email= $_REQUEST['email'];
-		$link= mysqli_connect("localhost","root","");
-		mysqli_select_db($link,"TeacherMatch");
-		$ins= mysqli_query($link,"Insert (usuario)");*/
-	?>
-</body>
-</html>
+<?php
+	echo "kk<br>";
+	if(isset($_REQUEST['enviarR'])){
+	$nombre= $_REQUEST['nombre'];
+	$apellidoP= $_REQUEST['apellidoP'];
+	$apellidoM= $_REQUEST['apellidoM'];
+	$carrera= $_REQUEST['carrera'];	
+	$porcentaje= $_REQUEST['porcentaje'];
+	$user= $_REQUEST['usuario'];
+	$email= $_REQUEST['email'];
+	$pass= $_REQUEST['contrasena'];
+	echo "$user, $pass, $nombre, $apellidoP, $apellidoM, $carrera, $porentaje, $email";
+	$link= mysqli_connect("localhost","root","");
+	mysqli_select_db($link,"TeacherMatch");
+	$ins= mysqli_query($link,"Insert into alumnos (usuario, contrasena, nombre, apellidoP, apellidoM, carrera, porcentaje, email) values ($user, $pass, $nombre, $apellidoP, $apellidoM, $carrera, $porentaje, $email)");
+	mysqli_close($link);
+	}
+?>
