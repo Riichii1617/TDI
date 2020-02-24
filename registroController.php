349fc55a5ea386c02b40541d27cbf6b47e97d2df
Jesus-RA <jesus.ra98@hotmail.com>
2020-02-24 12:45:11 -0600

    Registro de usuario

--- registroController.php
+++ registroController.php
@@ -1,18 +1,37 @@
-<?php
-	echo "kk<br>";
-	if(isset($_REQUEST['nombre'])){
-	$nombre= $_REQUEST['nombre'];
-	$apellidoP= $_REQUEST['apellidoP'];
-	$apellidoM= $_REQUEST['apellidoM'];
-	$carrera= $_REQUEST['carrera'];	
-	$porcentaje= $_REQUEST['porcentaje'];
-	$user= $_REQUEST['usuario'];
-	$email= $_REQUEST['email'];
-	$pass= $_REQUEST['contrasena'];
-	echo "$user, $pass, $nombre, $apellidoP, $apellidoM, $carrera, $porentaje, $email";
-	$link= mysqli_connect("localhost","root","");
-	mysqli_select_db($link,"TeacherMatch");
-	$ins= mysqli_query($link,"Insert into alumnos (usuario, contrasena, nombre, apellidoP, apellidoM, carrera, porcentaje, email) values ($user, $pass, $nombre, $apellidoP, $apellidoM, $carrera, $porentaje, $email)");
-	mysqli_close($link);
+<?PHP	
+	if(isset($_REQUEST['enviarR'])){
+		$nombre= $_REQUEST['nombre'];
+		$apellidoP= $_REQUEST['apellidoP'];
+		$apellidoM= $_REQUEST['apellidoM'];
+		$carrera= $_REQUEST['carrera'];	
+		$porcentaje= $_REQUEST['porcentaje'];
+		$user= $_REQUEST['usuario'];
+		$email= $_REQUEST['email'];
+		$pass= $_REQUEST['contrasena'];	
+		$link= mysqli_connect("localhost","root","");//Conexion a la base de datos
+		mysqli_select_db($link,"TeacherMatch");//Selecciona la base de datos a usar
+		$unicidad= mysqli_query($link, "Select usuario, email from alumnos where usuario = '$user' or email = '$email';");//Consulta para verificar la unicidad de las cuentas
+		$row= mysqli_fetch_array($unicidad);
+		if($row['email'] ==  $email){		
+			echo "<script >
+			 alert('Email ya existe'); 
+			 window.history.back(-1);
+			 </script>";
+
+		}else if($row['usuario'] == $user){
+			echo "<script>
+			 		alert('Usuario ya existe'); 
+			 		window.history.back(-1);
+			 	</script>";
+		}
+		else {
+			if (mysqli_query($link,"Insert into alumnos (usuario, contrasena, nombre, apellidoP, apellidoM, carrera, porcentaje, email) values ('$user', '$pass', '$nombre', '$apellidoP', '$apellidoM', '$carrera',$porcentaje, '$email');") == true){
+				
+			}else{
+				echo "<script> alert('Consulta incorrecta'); </script>";
+			}
+			
+		}
+		mysqli_close($link);
 	}
 ?>
